b1756dac931aa3ce4bcd2d0a9651c4cae0512f7e
Victor <victorobioratech@gmail.com>
2023-05-05 00:17:11 +0100

    'adjusted added to help with build'

--- src/pages/play/startplaying.js
+++ src/pages/play/startplaying.js
@@ -1,7 +1,15 @@
 import GameScreen from "@/components/GameScreen";
+import { useSelector } from "react-redux";
+import { Fragment } from "react";
 
 const startplaying = () => {
-  return <GameScreen />;
+  const QuestionsArray = useSelector((state) => state.q.questions);
+
+
+  return <Fragment>
+    {QuestionsArray.length > 0 &&  <GameScreen />}
+  </Fragment>
+
 };
 
 export default startplaying;
